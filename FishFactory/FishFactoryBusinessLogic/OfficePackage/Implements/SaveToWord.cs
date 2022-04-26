ece01d792ba82bb1c6cb0da992efa8a569ebfd4b
d <dashka400g@gmail.com>
2022-04-27 01:15:03 +0400

    1

--- FishFactory/FishFactoryBusinessLogic/OfficePackage/Implements/SaveToWord.cs
+++ FishFactory/FishFactoryBusinessLogic/OfficePackage/Implements/SaveToWord.cs
@@ -3,10 +3,7 @@ using FishFactoryBusinessLogic.OfficePackage.HelperModels;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
-
 namespace FishFactoryBusinessLogic.OfficePackage.Implements
-
-
 {
     public class SaveToWord : AbstractSaveToWord
     {
@@ -77,44 +74,12 @@ namespace FishFactoryBusinessLogic.OfficePackage.Implements
         }
         protected override void CreateWord(WordInfo info)
         {
-            _wordDocument = WordprocessingDocument.Create(info.FileName, WordprocessingDocumentType.Document);
+            _wordDocument = WordprocessingDocument.Create(info.FileName,
+           WordprocessingDocumentType.Document);
             MainDocumentPart mainPart = _wordDocument.AddMainDocumentPart();
             mainPart.Document = new Document();
             _docBody = mainPart.Document.AppendChild(new Body());
         }
-       /* protected override void CreateParagraph(WordParagraph paragraph)
-        {
-            if (paragraph != null)
-            {
-                var docParagraph = new Paragraph();
-
-                docParagraph.AppendChild(CreateParagraphProperties(paragraph.TextProperties));
-                foreach (var run in paragraph.Texts)
-                {
-                    var docRun = new Run();
-                    RunProperties properties = new RunProperties();
-                    properties.AppendChild(new FontSize { Val = run.Item2.Size });
-                    // properties.AppendChild(new Bold);\
-                    RunProperties runProperties = new RunProperties();
-                    run.Item2.Bold = true;
-
-                    if (run.Item2.Bold)
-                    {
-                        properties.AppendChild(new Bold());
-                    }
-                    docRun.AppendChild(properties);
-                    docRun.AppendChild(new Text
-                    {
-                        Text = run.Item1,
-                        Space =
-                   SpaceProcessingModeValues.Preserve
-                    });
-                    docParagraph.AppendChild(docRun);
-                }
-                _docBody.AppendChild(docParagraph);
-            }
-        }*/
-
         protected override void CreateParagraph(WordParagraph paragraph)
         {
             if (paragraph != null)
@@ -135,14 +100,14 @@ namespace FishFactoryBusinessLogic.OfficePackage.Implements
                     docRun.AppendChild(new Text
                     {
                         Text = run.Item1,
-                        Space = SpaceProcessingModeValues.Preserve
+                        Space =
+                   SpaceProcessingModeValues.Preserve
                     });
                     docParagraph.AppendChild(docRun);
                 }
                 _docBody.AppendChild(docParagraph);
             }
         }
-
         protected override void SaveWord(WordInfo info)
         {
             _docBody.AppendChild(CreateSectionProperties());
